57ba14accc34878c069e22ac85e7e4ac63375696
tymanmimo <tymanmimo@gmail.com>
2023-09-12 02:18:22 -0500

    hoba

--- components/product.js
+++ components/product.js
@@ -6,11 +6,11 @@ export default function Product({ title, author, price, path }) {
         <>
             <div className={styles.container}>
                 <div className={styles.image_container}>
-                    <Image src={path} width={300} height={410} className={styles.image}/>
                     <div className={styles.image_overlay}>
                         <p className={styles.overlay_text}>{title}</p>
                         <p className={styles.overlay_text}>{author}</p>
                     </div>
+                    <Image src={path} width={300} height={410} className={styles.image}/>
                 </div>
                 <div className={styles.row}>
                     <h2 className={styles.price}>{price}$</h2>
